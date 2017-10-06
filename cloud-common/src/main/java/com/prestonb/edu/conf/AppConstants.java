e751163a597f18f4bef7eb17d7e82c9f3c3ca019
Preston <pbriggs28@users.noreply.github.com>
2017-10-05 17:20:46 -0700

    Extract dev users to common library

--- cloud-common/src/main/java/com/prestonb/edu/conf/AppConstants.java
+++ cloud-common/src/main/java/com/prestonb/edu/conf/AppConstants.java
@@ -1,5 +1,8 @@
 package com.prestonb.edu.conf;
 
+import com.prestonb.edu.user.domain.User;
+import com.prestonb.edu.user.domain.UserAuthenticationToken;
+
 public abstract class AppConstants {
 
 	/**
@@ -11,5 +14,44 @@ public abstract class AppConstants {
 		public static final String TASK_SERVICE = "task-service";
 		public static final String USER_SERVICE = "user-service";
 		public static final String WEBAPP = "webapp";
+	}
+	
+	public static abstract class DevUsers {
+		public static abstract class ValidUser1 {
+			private static final String FIRST_NAME = "Joshua";
+			private static final String LAST_NAME = "Bloch";
+			private static final String USERNAME = "admin";
+			private static final String PASSWORD = "CAFEBABE";
+
+			public static final User user;
+			public static final UserAuthenticationToken authToken;
+			static {
+				authToken = new UserAuthenticationToken(USERNAME, PASSWORD);
+				user = new User();
+				user.setFirstName(FIRST_NAME);
+				user.setLastName(LAST_NAME);
+				user.setUsername(USERNAME);
+				user.setPassword(PASSWORD);
+			}
+		}
+
+		public static abstract class InvalidUser1 {
+			private static final String FIRST_NAME = "Josh";
+			private static final String LAST_NAME = "Block";
+			private static final String USERNAME = "invalid";
+			private static final String PASSWORD = "invalid";
+
+			public static final User user;
+			public static final UserAuthenticationToken authToken;
+			static {
+				authToken = new UserAuthenticationToken(USERNAME, PASSWORD);
+				user = new User();
+				user.setFirstName(FIRST_NAME);
+				user.setLastName(LAST_NAME);
+				user.setUsername(USERNAME);
+				user.setPassword(PASSWORD);
+			}
+			
+		}
 	}
 }
--- cloud-user-service/src/main/java/com/prestonb/edu/api/UserService.java
+++ cloud-user-service/src/main/java/com/prestonb/edu/api/UserService.java
@@ -5,6 +5,7 @@ import javax.validation.Valid;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.prestonb.edu.conf.AppConstants.DevUsers;
 import com.prestonb.edu.user.api.UserApi;
 import com.prestonb.edu.user.domain.User;
 import com.prestonb.edu.user.domain.UserAuthenticationToken;
@@ -12,14 +13,8 @@ import com.prestonb.edu.user.domain.UserAuthenticationToken;
 @RestController
 public class UserService implements UserApi {
 	
-	private User adminUser;
-	{
-		adminUser = new User();
-		adminUser.setFirstName("Joshua");
-		adminUser.setLastName("Bloch");
-		adminUser.setUsername("admin");
-		adminUser.setPassword("CAFEBABE");
-	}
+	// TODO: Extract to DAO layer once built
+	private User adminUser = DevUsers.ValidUser1.user;
 
 	@Override
 	public User authenticate(@RequestBody @Valid UserAuthenticationToken creds) {
