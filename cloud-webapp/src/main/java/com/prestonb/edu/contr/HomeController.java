650e29a31a0984118d43972d7cedb8faf0a681b1
Preston <pbriggs28@users.noreply.github.com>
2017-10-05 17:37:32 -0700

    Converted to basic thymeleaf

--- cloud-webapp/src/main/java/com/prestonb/edu/contr/HomeController.java
+++ cloud-webapp/src/main/java/com/prestonb/edu/contr/HomeController.java
@@ -3,39 +3,41 @@ package com.prestonb.edu.contr;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
 
 import com.prestonb.edu.svc.UserServiceClient;
 import com.prestonb.edu.user.domain.User;
 import com.prestonb.edu.user.domain.UserAuthenticationToken;
 
-// TODO: Temporarily setup as rest before we convert to a front end
-@RestController
+@Controller
 public class HomeController {
 	@Autowired private UserServiceClient userServiceClient;
 
 	@RequestMapping()
-	public String home() {
-		return "Try: /login/admin/CAFEBABE";
+	public String home(Model model) {
+		model.addAttribute("userUrl", "/login/admin/CAFEBABE");
+		return "home";
 	}
 	
 	@RequestMapping(path = "/login/{username}/{password}", method = RequestMethod.GET)
-	public User loginPageGET(
+	public String loginPageGET(
+			Model model,
 			@PathVariable(name = "username", required = true) String username, 
 			@PathVariable(name = "password", required = true) String password) {
-		User user = loginPagePOST(new UserAuthenticationToken(username, password));
 		
-		return user;
+		return loginPagePOST(model, new UserAuthenticationToken(username, password));
 	}
 	
 	@RequestMapping(path = "/login", method = RequestMethod.POST)
-	public User loginPagePOST(@RequestBody @Valid UserAuthenticationToken token) {
+	public String loginPagePOST(Model model, @RequestBody @Valid UserAuthenticationToken token) {
 		User user = userServiceClient.authenticate(token);
+		model.addAttribute("user", user);
 
-		return user;
+		return "user";
 	}
 }
